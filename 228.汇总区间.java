db1eb3513d9fe8a0b0db155a420d3a398c766089
MrXWTe <WeitengXu@163.com>
2019-05-28 17:21:24 +0800

    update 2019/5/28

--- 228.汇总区间.java
+++ 228.汇总区间.java
@@ -5,6 +5,9 @@ import java.util.List;
  * @lc app=leetcode.cn id=228 lang=java
  *
  * [228] 汇总区间
+ * 
+ * 快慢指针法，仅快指针运动，当查找到不在区间的数时，快指针停止查找，将区间添加进list。
+ * 并且将慢指针指向快指针++后的位置，此时重新寻找区间
  */
 class Solution {
     public List<String> summaryRanges(int[] nums) {
