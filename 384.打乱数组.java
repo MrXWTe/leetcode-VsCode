d1b8ecd449be131180d0ab79c9e1714c01d4d980
MrXWTe <WeitengXu@163.com>
2019-07-29 09:49:07 +0800

    2019/07/29

---
+++ 384.打乱数组.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+/*
+ * @lc app=leetcode.cn id=384 lang=java
+ *
+ * [384] 打乱数组
+ *
+ * https://leetcode-cn.com/problems/shuffle-an-array/description/
+ *
+ * algorithms
+ * Medium (46.06%)
+ * Likes:    27
+ * Dislikes: 0
+ * Total Accepted:    8.8K
+ * Total Submissions: 18.9K
+ * Testcase Example:  '["Solution","shuffle","reset","shuffle"]\n[[[1,2,3]],[],[],[]]'
+ *
+ * 打乱一个没有重复元素的数组。
+ * 
+ * 示例:
+ * 
+ * 
+ * // 以数字集合 1, 2 和 3 初始化数组。
+ * int[] nums = {1,2,3};
+ * Solution solution = new Solution(nums);
+ * 
+ * // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。
+ * solution.shuffle();
+ * 
+ * // 重设数组到它的初始状态[1,2,3]。
+ * solution.reset();
+ * 
+ * // 随机返回数组[1,2,3]打乱后的结果。
+ * solution.shuffle();
+ * 
+ * 
+ */
+class Solution {
+
+    private int[] array;
+    private int[] original;
+
+    Random rand = new Random();
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public Solution(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+    
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+    
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
+        }
+        return array;
+    }
+
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+
