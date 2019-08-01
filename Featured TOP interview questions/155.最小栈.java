2f7971560f3f1c2cdea434c9db577d93a8316ba2
MrXWTe <WeitengXu@163.com>
2019-08-01 09:18:22 +0800

    2019/08/01

--- Featured TOP interview questions/155.最小栈.java
+++ Featured TOP interview questions/155.最小栈.java
@@ -51,6 +51,42 @@ class Element{
     }
 }
 
+
+class MinStack3{
+    private Stack<Integer> stack;
+    public MinStack3() {
+        stack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        if(stack.isEmpty()){
+            stack.push(x);
+            stack.push(x);
+        }else{
+            int tmp = stack.peek();
+            stack.push(x);
+            if(tmp<x){
+                stack.push(tmp);
+            }else{
+                stack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.get(stack.size()-2);
+    }
+    public int getMin() {
+        return stack.peek();
+    }
+}
+
+
 class MinStack2 {
 
     private Stack<Integer> data;
