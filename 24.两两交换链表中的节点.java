321ffcdd44fde45ac3f31da27b9053101ad77018
MrXWTe <WeitengXu@163.com>
2019-08-04 12:05:49 +0800

    2019/08/04

---
+++ 24.两两交换链表中的节点.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (61.23%)
+ * Likes:    252
+ * Dislikes: 0
+ * Total Accepted:    31.7K
+ * Total Submissions: 51.8K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+
+    /**
+     * 非递归
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs2(ListNode head) {
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode temp = res;
+        while(temp.next!=null && temp.next.next!=null){
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return res.next;
+    }
+}
+
